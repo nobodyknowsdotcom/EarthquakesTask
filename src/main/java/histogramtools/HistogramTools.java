43242f72045a3e03950a31d8cef3d9e6a54e2ad9
nobodyzhere <myclosestisa@gmail.com>
2021-12-20 19:26:00 +0500

    Added class to paint and save histogram to ./src directory [Working in testing mode]

--- src/main/java/histogramtools/HistogramTools.java
+++ src/main/java/histogramtools/HistogramTools.java
@@ -8,31 +8,30 @@ import org.jfree.chart.plot.PlotOrientation;
 
 public class HistogramTools {
     public static void main(String ... args) {
-        CreateHistogram("TestHistogram.png", "Name", "Price");
+        CreateHistogram("TestHistogram.png", "Name", "Price", 800, 500);
     }
 
 
-    public static void CreateHistogram(String name, String XAxis, String YAxis) {
+    public static void CreateHistogram(String name, String XAxis, String YAxis, int width, int height) {
         double[] value = new double[100];
         Random generator = new Random();
+        HistogramDataset dataset = new HistogramDataset();
+        String plotTitle = name.split("\\.")[0];
+        PlotOrientation orientation = PlotOrientation.VERTICAL;
+        dataset.setType(HistogramType.RELATIVE_FREQUENCY);
+        boolean show = false;
+        boolean toolTips = false;
+        boolean urls = false;
+        JFreeChart chart = ChartFactory.createHistogram(plotTitle, XAxis, YAxis,
+                dataset, orientation, show, toolTips, urls);
 
         for (int i=1; i < 100; i++) {
             value[i] = generator.nextDouble();
             int number = 10;
-            HistogramDataset dataset = new HistogramDataset();
-            dataset.setType(HistogramType.RELATIVE_FREQUENCY);
-            dataset.addSeries("Histogram",value,number);
-            String plotTitle = name.split("\\.")[0];
-            PlotOrientation orientation = PlotOrientation.VERTICAL;
-            boolean show = false;
-            boolean toolTips = false;
-            boolean urls = false;
-            JFreeChart chart = ChartFactory.createHistogram(plotTitle, XAxis, YAxis,
-                    dataset, orientation, show, toolTips, urls);
-            int width = 800;
-            int height = 500;
-            SaveHistogram(chart, name, width, height);
+            dataset.addSeries("Histogram", value, number);
         }
+
+        SaveHistogram(chart, name, width, height);
     }
 
     public static void SaveHistogram(JFreeChart chart, String name, int width, int height) {
@@ -41,5 +40,6 @@ public class HistogramTools {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.print("Histogram saved successfully!");
     }
 }
