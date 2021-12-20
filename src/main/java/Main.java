bb527fd0ffc727bb41ddb66c6b1dc85d05941bd0
nobodyzhere <myclosestisa@gmail.com>
2021-12-21 01:12:38 +0500

    Use HistogramTools functional to create ProductsAveragePrice histogram

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,34 @@
 import csvtools.CsvTools;
 import db.*;
-
+import histogramtools.HistogramTools;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
         /*
-          Подключается к базе данных catalog и создает таблицу Products, если она еще не создана.
-          Заполняет таблицу данными из catalog.csv.
+         * Подключается к базе данных по пути ./catalog.sqlite и парсит в Products все из ./catalog.csv
+         * Если в БД уже существует таблица Product, то она удаляется и заполняется данными из ./catalog.csv
+         * CreateHistogram создает гистограмму со средними ценами товаров, представленных в БД несколькими видами
          */
-        DbConnection connect = new DbConnection();
-        var connection = connect.getConnection();
+        var connection = DbConnection.getConnection("jdbc:sqlite:./catalog.sqlite");
         CsvTools csv = new CsvTools(connection);
         try {
             var data = CsvTools.ParseProductCsv("./catalog.csv");
             DbTools.updateProducts(connection, data);
-        } catch (IOException e) {
+
+            HistogramTools.CreateHistogram("ProductsAveragePrice.png",
+                    DbConnection.getConnection("jdbc:sqlite:./catalog.sqlite"),
+                    "Товары",
+                    "Средняя цена, р.",
+                    800,
+                    500);
+
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (IOException | SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
